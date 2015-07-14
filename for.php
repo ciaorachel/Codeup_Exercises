9451756ec988530f162c501bdb0ec9890bda473a
ciaorachel <ciaorachel@gmail.com>
2015-07-14 11:56:55 -0500

    added is_numeric to check inputs before running for-loop

--- for.php
+++ for.php
@@ -1,25 +1,24 @@
 <?php  
 
 fwrite(STDOUT, 'Key in a starting number, then hit Enter ');
-
 $b = trim(fgets(STDIN));
 
 fwrite(STDOUT, 'Now pick the ending number and hit Enter ');
-
 $c = trim(fgets(STDIN));
 
 fwrite(STDOUT, 'Now select the increment to count by and hit Enter ');
-
 $d = trim(fgets(STDIN));
-if ($d == "") {
+if ($d == "" || $d == 0) {
 	$d = 1;
 }
 
-
-for ($a = $b; $a <= $c; $a += $d) {
+if (is_numeric($b) && is_numeric($c) && is_numeric($d)) {
+	for ($a = $b; $a <= $c; $a += $d) {
 	echo "\$a has a value of {$a}\n";
+	}
+
+} else {
+	fwrite(STDOUT, 'Not valid input. Start over... ');
 }
-
-
 
 ?>
