942c22fc8c7561f0d2480e578b4ff56f904305bf
ciaorachel <ciaorachel@gmail.com>
2015-07-14 15:00:54 -0500

    commented out the other code to make it display as asked in the exercise

--- foreach.php
+++ foreach.php
@@ -4,7 +4,7 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-
+/*
 foreach($things as $thing) {
 	if (is_int($thing)) {
 		echo "Integer\n";
@@ -37,7 +37,7 @@ foreach($things as $thing) {
 	} else {
 		echo "{$thing} is not a scalar\n";
 	}
-}
+}*/
 
 foreach ($things as $thing) {
 	if (is_array($thing)) {
