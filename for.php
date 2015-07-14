b2b2da2609ff2f47bc950af18a505f6ad164f76e
ciaorachel <ciaorachel@gmail.com>
2015-07-14 11:25:34 -0500

    added increment variable with default to 1 if nothing entered

--- for.php
+++ for.php
@@ -1,6 +1,6 @@
 <?php  
 
-fwrite(STDOUT, 'Key in a starting number, then hit Enter');
+fwrite(STDOUT, 'Key in a starting number, then hit Enter ');
 
 $b = trim(fgets(STDIN));
 
@@ -8,7 +8,15 @@ fwrite(STDOUT, 'Now pick the ending number and hit Enter ');
 
 $c = trim(fgets(STDIN));
 
-for ($a = $b; $a <= $c; $a++) {
+fwrite(STDOUT, 'Now select the increment to count by and hit Enter ');
+
+$d = trim(fgets(STDIN));
+if ($d == "") {
+	$d = 1;
+}
+
+
+for ($a = $b; $a <= $c; $a += $d) {
 	echo "\$a has a value of {$a}\n";
 }
 
