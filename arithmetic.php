622549d4e4444ceec1e933dcc71ca91b21a7b7f8
ciaorachel <ciaorachel@gmail.com>
2015-07-15 11:36:29 -0500

    completed problems in the arithmetic exercise

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,35 @@
 <?php
 
+$x = 2;
+$y = 1;
+
 function add($a, $b)
 {
-    echo $a + $b;
+    return $a + $b;
 }
+
+echo add($x, $y) . PHP_EOL;
+echo add(3, 4) . PHP_EOL;
 
 function subtract($a, $b)
 {
-    // Add code here
+    return $a - $b;
 }
+echo subtract($x, $y) . PHP_EOL;
+echo subtract(3, 4) . PHP_EOL;
 
 function multiply($a, $b)
 {
-    // Add code here
+    return $a * $b;
 }
+echo multiply($x, $y) . PHP_EOL;
+echo multiply(3, 4) . PHP_EOL;
 
 function divide($a, $b)
 {
-    // Add code here
+    return $a / $b;
 }
+echo divide($x, $y) . PHP_EOL;
+echo divide(3, 4) . PHP_EOL;
 
 // Add code to test your functions here
