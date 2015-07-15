85e2596bc4e5f4d9d45fe776d6f4c1dc5f425585
ciaorachel <ciaorachel@gmail.com>
2015-07-15 13:04:50 -0500

    added if-else to functions to confirm that parameters are numbers

--- arithmetic.php
+++ arithmetic.php
@@ -6,40 +6,63 @@ $y = 1;
 $a = 5;
 $b = 10;
 
-function add($a, $b)
-{
-    return $a + $b;
+//Addition function - checks for numbers
+function add($a, $b){
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a + $b;
+	} else {
+		return "ERROR: Both arguments must be numbers" . PHP_EOL; 
+	}
 }
-
 echo add($x, $y) . PHP_EOL;
 echo add(6, 4) . PHP_EOL;
 echo add($b, 3) . PHP_EOL;
 
-function subtract($a, $b)
-{
-    return $a - $b;
+
+//Subtraction function - checks for numbers
+function subtract($a, $b){
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a - $b;
+    } else {
+		return "ERROR: Both arguments must be numbers" . PHP_EOL; 
+	}
 }
 echo subtract($x, $y) . PHP_EOL;
 echo subtract(6, 4) . PHP_EOL;
 echo subtract($a, $y) . PHP_EOL;
 
-function multiply($a, $b)
-{
-    return $a * $b;
+
+//Multiplication function - checks for numbers
+function multiply($a, $b){
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a * $b; 
+    } else {
+		return "ERROR: Both arguments must be numbers" . PHP_EOL; 
+	}
 }
 echo multiply($x, $y) . PHP_EOL;
 echo multiply(6, 4) . PHP_EOL;
 
-function divide($a, $b)
-{
-    return $a / $b;
+
+//Division function - checks for numbers
+function divide($a, $b){
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a / $b;
+} else {
+		return "ERROR: Both arguments must be numbers" . PHP_EOL; 
+	}
 }
 echo divide($x, $y) . PHP_EOL;
 echo divide(6, 4) . PHP_EOL;
 
-function modulus($a, $b)
-{
-	return $a % $b;
+
+//Modulus function - checks for numbers
+function modulus($a, $b){
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a % $b;
+} else {
+		return "ERROR: Both arguments must be numbers" . PHP_EOL; 
+	}
 }
 echo modulus($x, $y) . PHP_EOL;
 echo modulus(6, 4) . PHP_EOL;
