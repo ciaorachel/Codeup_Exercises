292d8000461db69ab88f619cf54495d581d7a7eb
ciaorachel <ciaorachel@gmail.com>
2015-07-16 12:04:51 -0500

    created function that returns true or false if an array value is found. searched for tina and bob.

--- search-arrays.php
+++ search-arrays.php
@@ -4,4 +4,17 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
+//Create a function that returns TRUE or FALSE if an array value is found
+function isinArray($needle, $haystack){
+	$result = array_search($needle, $haystack);
+	if ($result !== false) {
+		return true;
+	} else {
+		return false; 
+	}
+}
+
+var_dump(isinArray('Bob', $names));
+var_dump(isinArray('Tina', $names));
+
 ?>
