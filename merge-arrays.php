438ce3f56474d9bf87d5c7809619dad68964bbed
ciaorachel <ciaorachel@gmail.com>
2015-07-17 14:54:43 -0500

    cleaned up code after code review

--- merge-arrays.php
+++ merge-arrays.php
@@ -2,7 +2,7 @@
 
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam', 'Jordan', 'Matt'];
 
-$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael', 'Jordan'];
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael', 'Jordan', 'Ben'];
 
 //Write a function called combine_arrays() that will take in two array values and return a new array with values from both.
 // use a combination of array_shift() & array_push() or array_pop() & array_unshift().
@@ -11,17 +11,15 @@ function combine_arrays($array1, $array2) {
 	$combinedArray = [];
 	foreach ($array1 as $array1Value) {
 		$array2Value = array_shift($array2);
-		if ($array1Value == $array2Value) {
-			array_push($combinedArray, $array1Value);
-		}
+		array_push($combinedArray, $array1Value);
+		
 		if ($array1Value !== $array2Value) {
-			array_push($combinedArray, $array1Value);
 			array_push($combinedArray, $array2Value);
 		}
 	}
-	print_r($combinedArray);	
+	return $combinedArray;	
 }
 
-combine_arrays($names, $compare);
+print_r(combine_arrays($names, $compare));
 
 ?>
