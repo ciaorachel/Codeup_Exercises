f12d56128106e57516f71ff37afca6b2076e44a5
ciaorachel <ciaorachel@gmail.com>
2015-07-17 15:15:06 -0500

    cleaned up code, cut other option for renaming keys as the option left on page is more succinct

--- contact-parser.php
+++ contact-parser.php
@@ -1,7 +1,6 @@
 <?php
 
-function parseContacts($filename)
-{
+function parseContacts($filename) {
     $handle = fopen($filename, 'r');
     $contents = trim(fread($handle, filesize($filename)));
     $contentsArray = explode(PHP_EOL, $contents);
@@ -10,11 +9,7 @@ function parseContacts($filename)
     	//Explode the contacts at pipe symbol
     	$contactArray = explode('|', $contact);
 
-    	//Option 1 for renaming keys
-    	//$contacts[$key]['name'] = $contactArray[0];
-    	//$contacts[$key]['number'] = formatNumber($contactArray[1]);
-
-    	//Option 2 for renaming keys
+    	//Renaming keys
     	$contacts[] = array(
     		'name' => $contactArray[0],
     		'number' => formatNumber($contactArray[1]),
@@ -24,9 +19,9 @@ function parseContacts($filename)
     return $contacts;
 }
 
+//Function for formatting phone number
 function formatNumber ($number) {
 	return substr($number, 0, 3) .'-' . substr($number, 3, 3) . '-' . substr($number, 6, 4);
 }
-
 
 var_dump(parseContacts('contacts.txt'));
