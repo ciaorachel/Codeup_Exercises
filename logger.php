5929e4ac86bf7a03420fa742e7d35b1105e5b0e8
ciaorachel <ciaorachel@gmail.com>
2015-07-20 11:16:53 -0500

    added log info and log error functions that call log message function, so it passes appropriate log level values -- makes it more reusable

--- logger.php
+++ logger.php
@@ -5,11 +5,25 @@ date_default_timezone_set('America/Chicago');
 function logMessage($logLevel, $message) {
 	$todaysDate = date('Y-m-d');
 	$timeNow = date('H:i:s');
+	$stringToWrite = "$todaysDate $timeNow [{$logLevel}] $message"; 
 	$filename = 'txt/log-' . $todaysDate . '.log';
 	$handle = fopen($filename, 'a+');
-	fwrite($handle, $todaysDate . " " . $timeNow . " " . $logLevel . " " . $message . PHP_EOL);
+	fwrite($handle, PHP_EOL . $stringToWrite);
 	fclose($handle);
 }
 
-logMessage("INFO", "This is an info message.") . PHP_EOL;
-logMessage("ERROR", "This is an error message.") . PHP_EOL;
+//logMessage("INFO", "This is an info message.") . PHP_EOL;
+//logMessage("ERROR", "This is an error message.") . PHP_EOL;
+
+// Add logInfo() and logError() functions that call logMessage(), passing the appropriate log level values. Replace the calls to logMessage() with calls to the new functions you just created.
+
+function logInfo($message) {
+	return logMessage('INFO', $message);
+}
+
+function logError($message) {
+	return logMessage('ERROR', $message);
+}
+
+logError("This is a 405 ERROR") . PHP_EOL;
+logInfo("here's a custom info message. greater reusability. woohoo!") . PHP_EOL;
