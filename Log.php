e93ac2df838b5b5ab85d0f524af470a505a6e61a
ciaorachel <ciaorachel@gmail.com>
2015-07-23 16:13:31 -0500

    completed exercise on php classes and objects, refactoring logger.php code

---
+++ Log.php
@@ -0,0 +1,25 @@
+<?php  
+
+class Log 
+{
+	public $filename;
+	public function logMessage($logLevel, $message) {
+		date_default_timezone_set('America/Chicago');
+		$todaysDate = date('Y-m-d');
+		$timeNow = date('H:i:s');
+		$stringToWrite = "$todaysDate $timeNow [{$logLevel}] $message"; 
+		$handle = fopen($this->filename, 'a');
+		fwrite($handle, PHP_EOL . $stringToWrite);
+		fclose($handle);
+	}
+
+	public function info($message) {
+		return $this->logMessage('INFO', $message);
+	}	
+
+	public function error($message) {
+		return $this->logMessage('ERROR', $message);
+	}
+}
+
+?>
