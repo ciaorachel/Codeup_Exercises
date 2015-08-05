75986fb60b0c19b50b464d0d716fc75965dab15b
ciaorachel <ciaorachel@gmail.com>
2015-08-05 13:11:27 -0500

    tweaked code to follow along in exercise on visibility

--- shapes_test.php
+++ shapes_test.php
@@ -5,15 +5,11 @@ require_once 'square.php';
 
 $rectangle = new Rectangle(2, 3);
 echo 'Rectangle area is:' . $rectangle->area() . PHP_EOL;
+//echo 'Rectangle perimeter is:' . $rectangle->perimeter() . PHP_EOL;
 
-/*
-//This is my echo for Square class that extends Rectangle, however, we've refactored this for improved usage.
-$square = new Square(2, 2);
+
+$square = new Square(2);
 echo 'Square area is:' . $square->area() . PHP_EOL;
-echo 'Square perimeter is:' . $square->perimeter() . PHP_EOL;
-*/
-
-$square2 = new Square(2);
-echo 'Square perimeter is:' . $square2->perimeter() . PHP_EOL;
+//echo 'Square perimeter is:' . $square->perimeter() . PHP_EOL;
 
 ?>
