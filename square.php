f27919a29debcbbd56d1911d39caf4073c466260
ciaorachel <ciaorachel@gmail.com>
2015-08-05 11:09:33 -0500

    updated square class to override parent's constructor, only accepting one attribute (height) and using that to set both width and height in square class.

--- square.php
+++ square.php
@@ -1,34 +1,24 @@
 <?php 
 
-/*require_once 'rectangle.php';
+require_once 'rectangle.php';
 
 
 class Square extends Rectangle
 {	
-	public function __construct($height, $width)
+	public function __construct($height)
 	{
-		$this->height = $height;
-		$this->width = $width;
+		parent::__construct($height, $height);
+	}
+
+	public function area()
+	{
+		$area = $this->height * 4;
+		return $area;
 	}
 
 	public function perimeter() 
 	{	
-		$perimeter = ($this->height * 2) + ($this->width * 2);
-		return $perimeter;
-	}
-
-}*/
-
-class Square
-{	
-	public function __construct($length)
-	{
-		$this->length = $length;
-	}
-
-	public function perimeter() 
-	{	
-		$perimeter = ($this->length * 4);
+		$perimeter = $this->height * 4;
 		return $perimeter;
 	}
 
