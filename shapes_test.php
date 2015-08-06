fffeca76deb076ee578428e2037e76cd069d8bc6
ciaorachel <ciaorachel@gmail.com>
2015-08-06 08:55:56 -0500

    added back in commented out code to see all my echoes

--- shapes_test.php
+++ shapes_test.php
@@ -4,12 +4,12 @@ require_once 'rectangle.php';
 require_once 'square.php';
 
 $rectangle = new Rectangle(2, 3);
-echo 'Rectangle area is:' . $rectangle->area() . PHP_EOL;
-//echo 'Rectangle perimeter is:' . $rectangle->perimeter() . PHP_EOL;
+echo 'Rectangle area is: ' . $rectangle->area() . PHP_EOL;
+echo 'Rectangle perimeter is: ' . $rectangle->perimeter() . PHP_EOL;
 
 
-$square = new Square(2);
-echo 'Square area is:' . $square->area() . PHP_EOL;
-//echo 'Square perimeter is:' . $square->perimeter() . PHP_EOL;
+$square = new Square(5);
+echo 'Square area is: ' . $square->area() . PHP_EOL;
+echo 'Square perimeter is: ' . $square->perimeter() . PHP_EOL;
 
 ?>
