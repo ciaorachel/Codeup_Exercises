9fdab10cbca026a9164b2fce9717f75da3fd3322
ciaorachel <ciaorachel@gmail.com>
2015-08-06 08:55:01 -0500

    pared down Square class, modifying constructor now that Rectangle class has setters and getters

--- square.php
+++ square.php
@@ -8,20 +8,7 @@ class Square extends Rectangle
 	public function __construct($height)
 	{
 		parent::__construct($height, $width = $height);
-		$this->height = $height;
-	}
-
-
-	public function area()
-	{
-		$area = $this->height * $this->height;
-		return $area;
-	}
-
-	public function perimeter() 
-	{	
-		$perimeter = ($this->height * 2) + ($this->height * 2);
-		return $perimeter;
+		
 	}
 
 }
